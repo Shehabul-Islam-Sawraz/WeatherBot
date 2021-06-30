263a79f6400d5284248ea4d627d7b859fce1f26f
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-06-30 13:11:51 +0600

    Callback added for echoing user's given text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Check for new messages -> polling
 updater = Updater(token="1707166632:AAEYOQDCQU3ltuGpbjIhz43f1TefK_5QEW4")
@@ -15,6 +15,15 @@ start_handler = CommandHandler("start",start)
 
 # Add command handler to dispatcher
 dispatcher.add_handler(start_handler)
+
+# Define a callback function for doing echo to any other command than '/start'
+def echo(update, context):
+    context.bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
+# Handler for the 'echo' function
+echo_handler = MessageHandler(Filters.text, echo) 
+
+dispatcher.add_handler(echo_handler)
 
 # Start polling for weather
 updater.start_polling()
